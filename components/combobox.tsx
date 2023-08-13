78a9412ff151aefc6dde3bfb46cadbb6f9a0f6ab
oncet <camilorivera86@gmail.com>
2023-08-12 23:26:17 -0300

    feat: form

---
+++ components/combobox.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const seasons = [
+  {
+    value: "all",
+    label: "All seasons",
+  },
+  ...Array.from({ length: 33 }, (_, i) => ({
+    value: `season-${i + 1}`,
+    label: `Season ${i + 1}`,
+  })),
+];
+
+export function ComboboxDemo() {
+  const [open, setOpen] = React.useState(false);
+  const [selectedOptions, setSelectedOptions] = React.useState<string[]>([
+    "all",
+  ]);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between"
+        >
+          {selectedOptions.length > 0
+            ? seasons
+                .filter((framework) =>
+                  selectedOptions.includes(framework.value)
+                )
+                .map((framework) => framework.label)
+                .join(", ")
+            : "Select seasons..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0 w-[--radix-popover-trigger-width]">
+        <Command>
+          <CommandInput placeholder="Search seasons..." />
+          <CommandEmpty>No season found.</CommandEmpty>
+          <CommandGroup>
+            {seasons.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={(currentValue) => {
+                  if (selectedOptions.includes(currentValue)) {
+                    if (currentValue !== "all") {
+                      setSelectedOptions(
+                        selectedOptions.filter(
+                          (option) => option !== currentValue
+                        )
+                      );
+                    }
+                  } else {
+                    if (currentValue === "all") {
+                      setSelectedOptions(["all"]);
+                    } else {
+                      setSelectedOptions([
+                        ...selectedOptions.filter(
+                          (selectedOption) => selectedOption !== "all"
+                        ),
+                        currentValue,
+                      ]);
+                    }
+                  }
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    selectedOptions.includes(framework.value)
+                      ? "opacity-100"
+                      : "opacity-0"
+                  )}
+                />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
