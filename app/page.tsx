a07e6d43d29c56f4d6ee0770276adf50e052dac7
oncet <camilorivera86@gmail.com>
2023-08-27 11:41:21 -0300

    catch error

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,37 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { AlertCircle, FileWarning, Terminal } from "lucide-react";
+
 import Form, { Episode } from "./form";
+import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
 
 export default function Home() {
   const [episode, setEpisode] = useState<Episode>();
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const onSubmit = async (selectedSeasons?: string) => {
     setIsLoading(true);
+    setError(false);
 
-    const res = await fetch("/get-random-episode", {
-      method: "POST",
-      body: JSON.stringify({
-        seasons: selectedSeasons,
-      }),
-    });
+    try {
+      const res = await fetch("/get-random-episode", {
+        method: "POST",
+        body: JSON.stringify({
+          seasons: selectedSeasons,
+        }),
+      });
 
-    const episode = await res.json();
+      const episode = await res.json();
 
-    setIsLoading(false);
+      setIsLoading(false);
 
-    setEpisode(episode);
+      setEpisode(episode);
+    } catch (error) {
+      setIsLoading(false);
+      setError(true);
+    }
   };
 
   return (
@@ -29,14 +39,32 @@ export default function Home() {
       <Form onSubmit={onSubmit} isLoading={isLoading} />
       <div
         className={
-          "transition duration-300 mt-4   " +
+          "transition duration-300 mt-4 " +
           (episode ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8")
         }
       >
         <div className={isLoading ? "animate-pulse" : ""}>
-          <h2 className="text-5xl font-extrabold">Moaning Lisa</h2>
-          <p className="dark:text-slate-400 mt-2">S01E06</p>
+          {episode ? (
+            <>
+              <h2 className="text-5xl font-extrabold">Moaning Lisa</h2>
+              <p className="dark:text-slate-400 mt-2">S01E06</p>
+            </>
+          ) : (
+            ""
+          )}
         </div>
+      </div>
+      <div
+        className={
+          "transition duration-300 mt-4 " +
+          (error ? "opacity-100 translate-y-0" : "opacity-0 translate-y-8")
+        }
+      >
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Failed to get a random episode</AlertTitle>
+          <AlertDescription>Please try again.</AlertDescription>
+        </Alert>
       </div>
     </main>
   );
