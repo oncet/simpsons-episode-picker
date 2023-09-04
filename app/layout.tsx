c1818974ce7c6e39636def73999153b28f2b8e1f
oncet <camilorivera86@gmail.com>
2023-09-03 22:21:10 -0300

    add header animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,11 @@ export default function RootLayout({
           <header className="border-b">
             <div className="dark:text-slate-400 mx-auto max-w-screen-sm px-4 py-2 flex items-center justify-between">
               <h1>
-                <Link href="/" className="flex items-center">
-                  <Tv className="mr-2 h-4 w-4 text-blue-600 dark:text-yellow-400" />{" "}
+                <Link
+                  href="/"
+                  className="flex items-center text-slate-500 hover:text-slate-800 hover:dark:text-white group transition"
+                >
+                  <Tv className="mr-2 h-4 w-4 text-blue-600 dark:text-yellow-400 group-hover:scale-125 group-hover:-rotate-12 transition" />{" "}
                   Simpsons episode picker
                 </Link>
               </h1>
