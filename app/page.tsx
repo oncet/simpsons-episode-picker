abec0520e86e3a1b627817c8c7cde5cf47a76ffe
oncet <camilorivera86@gmail.com>
2023-10-03 22:21:29 -0300

    use conditional randering

--- app/page.tsx
+++ app/page.tsx
@@ -37,37 +37,41 @@ export default function Home() {
   return (
     <main className="m-auto max-w-screen-sm p-4">
       <Form onSubmit={onSubmit} isLoading={isLoading} />
-      <div
-        className={
-          "mt-4 transition duration-300 " +
-          (episode ? "translate-y-0 opacity-100" : "translate-y-8 opacity-0")
-        }
-      >
-        <div className={isLoading ? "animate-pulse" : ""}>
-          {episode ? (
-            <>
-              <h2 className="text-5xl font-extrabold dark:text-white">
-                {episode.title}
-              </h2>
-              <p className="mt-2 dark:text-slate-400">{episode.code}</p>
-            </>
-          ) : (
-            ""
-          )}
+      {episode ? (
+        <div
+          className={
+            "mt-4 " + (episode ? "animate-fade-in" : "animate-fade-out")
+          }
+        >
+          <div className={isLoading ? "animate-pulse" : ""}>
+            {episode ? (
+              <>
+                <h2 className="text-5xl font-extrabold dark:text-white">
+                  {episode.title}
+                </h2>
+                <p className="mt-2 dark:text-slate-400">{episode.code}</p>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
-      </div>
-      <div
-        className={
-          "mt-4 transition duration-300 " +
-          (error ? "translate-y-0 opacity-100" : "translate-y-8 opacity-0")
-        }
-      >
-        <Alert variant="destructive">
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Failed to get a random episode</AlertTitle>
-          <AlertDescription>Please try again.</AlertDescription>
-        </Alert>
-      </div>
+      ) : (
+        ""
+      )}
+      {error ? (
+        <div
+          className={"mt-4 " + (error ? "animate-fade-in" : "animate-fade-out")}
+        >
+          <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>Failed to get a random episode</AlertTitle>
+            <AlertDescription>Please try again.</AlertDescription>
+          </Alert>
+        </div>
+      ) : (
+        ""
+      )}
     </main>
   );
 }
